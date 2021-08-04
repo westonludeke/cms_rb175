3ecf0c27ece9bdcc27eabec7b4845b48f5371720
westonludeke <westonludeke@gmail.com>
2021-08-04 10:24:46 -0500

    updated files.erb

--- cms.rb
+++ cms.rb
@@ -1,6 +1,17 @@
 require "sinatra"
 require "sinatra/reloader"
 
+helpers do 
+  def get_list_of_files
+    @files = Dir.entries('data')
+    @files
+  end
+end
+
 get "/" do 
-  "Getting started"
+  redirect "/files"
+end
+
+get "/files" do 
+  erb :files
 end
