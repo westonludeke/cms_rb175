8467bcc4f2b1b9e4ddfa6fae48fd1c6e160b49ed
westonludeke <westonludeke@gmail.com>
2021-08-04 12:16:02 -0500

    added new get routes and syntax to read contents of txt files

--- cms.rb
+++ cms.rb
@@ -5,8 +5,22 @@ require "tilt/erubis"
 root = File.expand_path("..", __FILE__)
 
 get "/" do
-  @files = Dir.glob(root + "/data/*").map do |path|
-    File.basename(path)
-  end
+  @files = Dir.glob(root + "/data/*").map { |path| File.basename(path) }
+  @files.sort!
   erb :index
+end
+
+get "/about.txt" do 
+  @contents = File.read("data/about.txt")
+  erb :page
+end
+
+get "/changes.txt" do 
+  @contents = File.read("data/changes.txt")
+  erb :page
+end
+
+get "/history.txt" do 
+  @contents = File.read("data/history.txt")
+  erb :page
 end
