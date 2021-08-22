7fd28150b2ea5f0ce4a5d9d23904f1de188904e5
westonludeke <westonludeke@gmail.com>
2021-08-22 13:06:55 -0500

    updated cms.rb

--- cms.rb
+++ cms.rb
@@ -5,9 +5,18 @@ require "tilt/erubis"
 
 root = File.expand_path("..", __FILE__)
 
+## test 
+
 get "/" do 
   @files = Dir.glob(root + "/data/*").map do |path|
     File.basename(path)
   end
   erb :index
+end
+
+get "/:file_name" do
+  file_path = root + "/data/" + params[:file_name]
+
+  headers["Content-Type"] = "text/plain"
+  File.read(file_path)
 end
